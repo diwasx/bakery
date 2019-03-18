a885e3434588e637ae95b05208f409c4ac88eefc
diwasx <diwasx@gmail.com>
2019-03-18 09:07:26 +0545

    Commented AppService

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,10 +14,10 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        $this -> app -> bind('path.public', function()
-{
-        return base_path('public_html');
-});
+        /* $this -> app -> bind('path.public', function() */
+        /* { */
+        /*         return base_path('public_html'); */
+        /* }); */
     }
 
     /**
